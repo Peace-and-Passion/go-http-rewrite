292453693a0c5c8968ff94dd4c47850bf09f6a63
thisisaaroland <aaron@mapzen.com>
2017-09-22 09:10:55 -0700

    initial commit

---
+++ rewrite.go
@@ -0,0 +1,95 @@
+package rewrite
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+type RewriteRule interface {
+	Match(string) bool
+	Transform(string) string
+	Last() bool
+}
+
+type RewriteRulesOptions map[string]interface{}
+
+func DefaultRewriteRulesOptions() RewriteRulesOptions {
+
+	opts := RewriteRulesOptions{
+		Last: false,
+	}
+
+	return opts
+}
+
+type RegexpRewriteRule struct {
+	RewriteRule
+	regexp  *regexp.Regexp
+	replace string
+	is_last bool
+}
+
+func (rule *RegexpRewriteRule) Match(path string) bool {
+	return rule.regexp.MatchString(path)
+}
+
+func (rule *RegexpRewriteRule) Transform(path string) string {
+	return rule.regexp.ReplaceAllString(path, rw.Replace)
+}
+
+func (rule *RegexpRewriteRule) Last() bool {
+	return rule.is_last
+}
+
+func RemovePrefixRewriteRule(path string, opts RewriteRulesOptions) RewriteRule {
+
+	pat := fmt.Sprintf("^%s(.*)", path)
+	re := regexp.MustCompile(pat)
+
+	is_last := false
+
+	_, ok := opts["Last"]
+
+	if ok {
+		is_last = opts["Last"].(bool)
+	}
+
+	rule := RewriteRule{
+		regexp:  re,
+		replace: "$1",
+		is_last: is_last,
+	}
+
+	return rule
+}
+
+func RewriteHandler(rules []RewriteRule, next http.Handler) (http.Handler, error) {
+
+	fn := func(rsp http.ResponseWriter, req *http.Request) {
+
+		path := req.URL.Path
+
+		for _, rw := range rules {
+
+			if !rw.Match(path) {
+				continue
+			}
+
+			path = re.Transform(path)
+
+			if rw.Last() {
+				break
+			}
+		}
+
+		req.URL.Path = path
+
+		next.ServeHTTP(rsp, req)
+		return
+	}
+
+	h := http.HandlerFunc(fn)
+	return h, nil
+}
